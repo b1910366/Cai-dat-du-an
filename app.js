86427bde6cb1c648274351c79b9d7c76c99d06d5
b1910366 <duocb1910366@student.ctu.edu.vn>
2023-02-23 09:22:36 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express =  require("express");
 const cors = require("cors");
 const req = require("express/lib/request");
 
-const contactsRouter = require("./app/routes/contact.route");
+
+const ApiError = require("./app/api-error");
 
 const app = express();
+
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
@@ -14,5 +17,16 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+    });
+    
+    app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json ({
+    message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -5,15 +5,15 @@ const router = express.Router();
 
 router.route("/")
     .get(contacts.findAll)
-    .get(contacts.create)
-    .get(contacts.deleteAll);
+    .post(contacts.create)
+    .delete(contacts.deleteAll);
 
 router.route("/favorite")
     .get(contacts.findAllFavorite)
 
 router.route("/:id")
     .get(contacts.findOne)
-    .get(contacts.update)
-    .get(contacts.delete);
+    .put(contacts.update)
+    .delete(contacts.delete);
 
-    module.exports = router;
+module.exports = router;
